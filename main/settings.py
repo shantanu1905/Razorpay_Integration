f39a6b95f105a2fc22aba3b1a2c44061772e1c77
shantanu1905 <nimkarshantanu07@gmail.com>
2021-09-08 23:46:04 +0530

    11.46

--- main/settings.py
+++ main/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['razorpay-djangoo.herokuapp.com']
 
