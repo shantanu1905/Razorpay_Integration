9ec2310ccc982f0e3981eebe00888b821c0e908f
shantanu1905 <nimkarshantanu07@gmail.com>
2021-09-08 23:01:06 +0530

    done

--- main/wsgi.py
+++ main/wsgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
 
-application = get_wsgi_application()
+#application = get_wsgi_application()    to use  with local host
+application = Cling(get_wsgi_application())
