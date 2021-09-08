e1010f1dc562c7e97b7d113263f03eff1bff1ab4
shantanu1905 <nimkarshantanu07@gmail.com>
2021-09-08 23:08:59 +0530

    heruko static error

--- main/settings.py
+++ main/settings.py
@@ -120,10 +120,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT='/static/'
-#STATICFILES_DIRS = [   os.path.join(BASE_DIR, 'static')]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field.
 
